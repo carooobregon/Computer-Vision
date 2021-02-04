7f44f7aad3485dbb5c83230940f194ef01fb7d28
Raul Herrera <raul_ehs@hotmail.com>
2021-02-04 04:12:39 -0600

    Add realtime sentiment plot script and sentiment data on Trump

--- NLP_Twitter/twdata.py
+++ NLP_Twitter/twdata.py
@@ -2,9 +2,11 @@ from flask import Flask, request, render_template
 import tweepy as tw
 import sentiment_mod as s
 from tweepy import OAuthHandler
+#consumer key, consumer secret, access token, access secret.
+from twitterkeys import *
 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_key, access_secret)
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
 api = tw.API(auth)
 
 stwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now", "like"]
